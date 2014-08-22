73d92bdf22d2017de83ac84c5ad92b7fb205437b
IonCannon218 <IonCannon21892@gmail.com>
2014-08-22 12:30:05 -0400

    Add laser control

--- spirograph_pwm.ino
+++ spirograph_pwm.ino
@@ -1,20 +1,20 @@
-// set up motor pins
+// set up pins
 int motor1Pin = 3;
 int motor1Val = 255;
 int motor2Pin = 9;
 int motor2Val = 255;
 int motor3Pin = 10;
 int motor3Val = 255;
-int motor4Pin = 11;
-int motor4Val = 255;
+
+int laserPin = 13;
 
 int delta = 5;      //delta limit for randomizer
-int minSpeed = 10;   // min PWM limit for motors
-int maxSpeed = 40;   // max PWM limit for motors
+int minSpeed = 50;   // min PWM limit for motors
+int maxSpeed = 150;   // max PWM limit for motors
 int dmin, dmax;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   
   Serial.println("Starting up");
   
@@ -22,7 +22,7 @@ void setup() {
   pinMode(motor1Pin, OUTPUT);
   pinMode(motor2Pin, OUTPUT);
   pinMode(motor3Pin, OUTPUT);
-  pinMode(motor4Pin, OUTPUT);
+  pinMode(laserPin, OUTPUT);
   
   // spin up motors
   int startSpeed = 30;
@@ -31,8 +31,6 @@ void setup() {
   analogWrite(motor2Pin, startSpeed);
   //delay(3000);
   analogWrite(motor3Pin, startSpeed);
-  //delay(3000);
-  analogWrite(motor4Pin, startSpeed);
   delay(3000);
   
   // set up random seed
@@ -42,41 +40,13 @@ void setup() {
   motor1Val = random(minSpeed, maxSpeed);
   motor2Val = random(minSpeed, maxSpeed);
   motor3Val = random(minSpeed, maxSpeed);
-  motor4Val = random(minSpeed, maxSpeed);
  
 }
 
-/*
 void loop() {
-  for (motor1Val = maxSpeed; motor1Val < minSpeed; motor1Val--) {
-    analogWrite(motor1Pin, motor1Val);
-  }
-  
-  for (motor2Val = maxSpeed; motor2Val < minSpeed; motor2Val--) {
-    analogWrite(motor2Pin, motor2Val);
-  }
-  
-  for (motor3Val = maxSpeed; motor3Val < minSpeed; motor3Val--) {
-    analogWrite(motor1Pin, motor3Val);
-  }
-        
-  for (motor4Val = maxSpeed; motor4Val < minSpeed; motor4Val--) {
-    analogWrite(motor1Pin, motor4Val);
-  }
-  
-  // print values to serial
-  Serial.print(motor1Val);
-  Serial.print(", ");
-  Serial.print(motor2Val);
-  Serial.print(", ");
-  Serial.print(motor3Val);
-  Serial.print(", ");
-  Serial.println(motor4Val);
-  delay(1000);
-}*/
-
-
-void loop() {
+  // turn off laser while changing motor speed
+  digitalWrite(laserPin, LOW);
+	
   // randomly change speed
   dmin = min(motor1Val - delta, minSpeed);
   dmax = max(motor1Val + delta, maxSpeed);
@@ -90,23 +60,46 @@ void loop() {
   dmax = max(motor3Val + delta, maxSpeed);
   motor3Val = random(dmin, dmax);
   
-  dmin = min(motor4Val - delta, minSpeed);
-  dmax = max(motor4Val + delta, maxSpeed);
-  motor4Val = random(dmin, dmax);
-  
   // output to motors
   analogWrite(motor1Pin, motor1Val);
   analogWrite(motor2Pin, motor2Val);
   analogWrite(motor3Pin, motor3Val);
-  analogWrite(motor4Pin, motor4Val);
+  
   // print values to serial
   Serial.print(motor1Val);
   Serial.print(", ");
   Serial.print(motor2Val);
   Serial.print(", ");
-  Serial.print(motor3Val);
-  Serial.print(", ");
-  Serial.println(motor4Val);
+  Serial.println(motor3Val);
   
-  delay(30000);
+  // code above executes too fast, add a delay
+  delay(250);
+  
+  // turn laser back on
+  digitalWrite(laserPin, HIGH);
+  
+  delay(3000);
 }
+
+/*
+// This interrupt is called every time timer0 counts up to the 8bit value
+// stored in the register "ORC0A", which is configured in INT0 interrupt.
+ISR(TIMER0_COMPA_vect) {
+	// turn laser off
+	if (laserOn == true) {
+		laserOn = false;
+		digitalWrite(laserPin, LOW);
+		
+		// Reset timer0
+		TCNT0 = 0;
+	} 
+	
+	// turn laser on
+	if (laserOn == false) {
+		laserOn = true;
+		digitalWrite(laserPin, HIGH);
+		
+		// Reset timer0
+		TCNT0 = 0;
+	}
+}*/
